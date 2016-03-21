d0b1125c5fc2653697bb1c53f2ff1427f56d4cdf
Luke Hoersten <luke@hoersten.org>
2016-03-21 17:48:28 -0500

    Added path parameter for metrics endpoint.

--- src/System/Metrics/Prometheus/Http.hs
+++ src/System/Metrics/Prometheus/Http.hs
@@ -2,7 +2,7 @@
 
 module System.Metrics.Prometheus.Http where
 
-
+import           Data.Text                                (Text)
 import           Network.HTTP.Types                       (hContentType,
                                                            methodGet, status200,
                                                            status404)
@@ -18,16 +18,23 @@ import           System.Metrics.Prometheus.GlobalRegistry (GlobalRegistry,
                                                            sample)
 
 
-serveHttpTextMetrics :: Port -> GlobalRegistry -> IO ()
-serveHttpTextMetrics port = run port . prometheusApp
+type Path = [Text]
 
 
-prometheusApp :: GlobalRegistry -> Application
-prometheusApp globalRegistry request respond
+serveHttpTextMetricsDef :: Port -> GlobalRegistry -> IO ()
+serveHttpTextMetricsDef = serveHttpTextMetrics ["metrics"]
+
+
+serveHttpTextMetrics :: Path -> Port -> GlobalRegistry -> IO ()
+serveHttpTextMetrics path port = run port . prometheusApp path
+
+
+prometheusApp :: Path -> GlobalRegistry -> Application
+prometheusApp path globalRegistry request respond
     | prometheusRequest = prometheusResponse respond globalRegistry
     | otherwise = respond $ responseLBS status404 header404 body404
   where
-    prometheusRequest = requestMethod request == methodGet && pathInfo request == ["metrics"]
+    prometheusRequest = requestMethod request == methodGet && pathInfo request == path
     header404 = [(hContentType, "text/plain")]
     body404 = "404"
 
@@ -36,4 +43,4 @@ prometheusResponse :: (Response -> IO b) -> GlobalRegistry -> IO b
 prometheusResponse respond gr =
     respond . responseBuilder status200 headers . encodeMetrics =<< sample gr
   where
-      headers = [(hContentType, "text/plain; version=0.0.4")]
+    headers = [(hContentType, "text/plain; version=0.0.4")]

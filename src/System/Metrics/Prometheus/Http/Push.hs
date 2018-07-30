22a85ab767ef20cd7f51701ac5c895884173b711
Luke Hoersten <luke@bitnomial.com>
2018-07-30 16:33:00 -0500

    Removed wreq dep to simplify deps and support older GHCs.

--- src/System/Metrics/Prometheus/Http/Push.hs
+++ src/System/Metrics/Prometheus/Http/Push.hs
@@ -10,13 +10,16 @@ import           Control.Monad                         (forever)
 import           Data.ByteString.Builder               (toLazyByteString)
 import           Data.Map                              (foldMapWithKey)
 import           Data.Text                             (Text, unpack)
-import           Network.HTTP.Client                   (Request,
+import           Network.HTTP.Client                   (Request (..),
                                                         RequestBody (..),
+                                                        defaultManagerSettings,
+                                                        httpNoBody, newManager,
                                                         requestBody,
+                                                        requestFromURI,
                                                         requestHeaders)
-import           Network.HTTP.Types                    (hContentType)
-import           Network.Wreq.Session                  (newSession, put)
-import           Network.Wreq.Types                    (Putable (..))
+import           Network.HTTP.Types                    (hContentType, methodPut)
+import           Network.URI                           (URI (..), URIAuth,
+                                                        nullURI)
 
 import           System.Metrics.Prometheus.Encode.Text (encodeMetrics)
 import           System.Metrics.Prometheus.MetricId    (Labels (..))
@@ -24,27 +27,27 @@ import           System.Metrics.Prometheus.Registry    (RegistrySample)
 
 
 -- | Push text metrics to a pushgateway.
-pushHttpTextMetrics :: String            -- ^ The base URL of the pushgateway, including the port number.
-                    -> Text              -- ^ The name of this job.
-                    -> Labels            -- ^ The label set to use as a grouping key for these metrics.
-                    -> Int               -- ^ Push frequency, in microseconds.
-                    -> IO RegistrySample -- ^ The action to get the latest metrics.
+pushHttpTextMetrics :: URIAuth           -- ^ PushGateway URI name, including port number (ex: myGateway.com:8080)
+                    -> Text              -- ^ Job name
+                    -> Labels            -- ^ Label set to use as a grouping key for metrics
+                    -> Int               -- ^ Microsecond push frequency
+                    -> IO RegistrySample -- ^ Action to get latest metrics
                     -> IO ()
-pushHttpTextMetrics base job (Labels ls) frequency getSample = do
-    session <- newSession
-    forever $ getSample >>= put session url >> threadDelay frequency
+pushHttpTextMetrics gatewayName jobName labels frequencyMicros getSample = do
+    manager    <- newManager defaultManagerSettings
+    requestUri <- requestFromURI $ buildUri gatewayName jobName labels
+    forever $ getSample >>= flip httpNoBody manager . request requestUri >> threadDelay frequencyMicros
   where
-    url = base ++ "/metrics/job/" ++ unpack job ++
-        foldMapWithKey (\k v -> "/" ++ unpack k ++ "/" ++ unpack v) ls
+    request req sample = req
+        { method         = methodPut
+        , requestBody    = RequestBodyLBS . toLazyByteString $ encodeMetrics sample
+        , requestHeaders = [(hContentType, "text/plain; version=0.0.4")]
+        }
 
-
-instance Putable RegistrySample where
-    putPayload = (pure .) . metricsRequest
-
-
-metricsRequest :: RegistrySample -> Request -> Request
-metricsRequest s req = req
-    { requestBody    = RequestBodyLBS . toLazyByteString $ encodeMetrics s
-    , requestHeaders = contentType : requestHeaders req
+buildUri :: URIAuth -> Text -> Labels -> URI
+buildUri gatewayName jobName (Labels ls) = nullURI
+    { uriScheme    = "http:"
+    , uriAuthority = Just gatewayName
+    , uriPath      = "/metrics/job/" ++ unpack jobName ++ foldMapWithKey labelPath ls
     }
-  where contentType = (hContentType, "text/plain; version=0.0.4")
+  where labelPath k v = "/" ++ unpack k ++ "/" ++ unpack v
